2157ef41d4c23b2f113e92b6b17e0f3d7aec706f
Tohami <tohami00076@gmail.com>
2023-10-17 05:24:06 -0700

    first commit

---
+++ Books/Models/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Books.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Author { get; set; }
+
+        [Required]
+        [MaxLength(1000)]
+        public string Description { get; set; }
+
+        public byte CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public DateTime AddedOn { get; set; }
+
+        public Book()
+        {
+            AddedOn = DateTime.Now;
+        }
+    }
+}
